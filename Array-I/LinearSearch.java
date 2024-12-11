60c46b088ab852f13a3c963bfcbf7fc08b7d4271
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2024-12-11 18:23:36 +0530

    LinearSerach

--- Array-I/LinearSearch.java
+++ Array-I/LinearSearch.java
@@ -1,5 +1,25 @@
 class LinearSearch{
+
+    public static int search(int arr[],int key){
+for (int i=0; i<arr.length; i++) {
+    if(arr[i]==key){
+return i;
+    }
+   
+}
+
+return -1;
+
+    }
     public static void main(String[] args) {
-        
+        int arr[]={15,46,78,11,89,852,14};
+        int key=89;
+       int ans= search(arr,key);
+       if(ans==-1){
+        System.out.println("The key is not Found");
+       }
+       else{
+        System.out.println("The key is at index: "+ans);
+       }
     }
 }
