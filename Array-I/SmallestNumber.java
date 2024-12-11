a15fb0cdc370abc8e75145a96cca9ae8cd37a86f
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2024-12-11 18:58:03 +0530

    SmallestNumber

---
+++ Array-I/SmallestNumber.java
@@ -0,0 +1,17 @@
+public class SmallestNumber {
+    public static int getSmallest(int arr[]){
+        int smallest=Integer.MAX_VALUE;
+        for (int i : arr) {
+            if(smallest>i){
+                smallest=i;
+            }
+        }
+        return smallest;
+    }
+    public static void main(String[] args) {
+        int arr[]={12,77,88,126,14,789,85,235,11,6,145,8466,523};
+        System.out.println("The smallest array inside the array is:"+ getSmallest(arr));
+        
+
+    }
+}
