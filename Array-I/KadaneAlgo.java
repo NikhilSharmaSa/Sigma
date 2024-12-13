f8103c8f324822602a496a73e09713c7a2127e55
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2024-12-13 10:10:37 +0530

    KadaneAlgo

---
+++ Array-I/KadaneAlgo.java
@@ -0,0 +1,21 @@
+public class KadaneAlgo {
+    public static int maxSum(int arr[]){
+int curr=0;
+int max=Integer.MIN_VALUE;
+for (int i : arr) {
+    curr+=i;
+    if(curr<0){
+        curr=0;
+    }
+    if(max<curr){
+        max=curr;
+    }
+}
+return max;
+    }
+    public static void main(String[] args) {
+        int [] arr={-2,-3,4,-1,-2,1,5,-3};
+       int ans= maxSum(arr);
+       System.out.println("The maxSubArraySum is :"+ans);
+    }
+}
