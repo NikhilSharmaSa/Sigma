7dbe7f61f1c90626027e15f55c784aad77855647
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2024-12-21 11:25:31 +0530

    DiagonalSumofaMatrix

---
+++ 2DArray/DiagonalSum.java
@@ -0,0 +1,18 @@
+public class DiagonalSum {
+
+    public static int calculateDiagonalSum(int matrix[][]){
+        int sum=0;
+        for(int i=0; i<matrix.length; i++){
+            sum+=matrix[i][i];
+            if(i!=matrix.length-1-i){
+                sum+=matrix[i][matrix.length-1-i];
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        int matrix[][]={{1,2,3},{4,5,6},{7,8,9}};
+       int ans= calculateDiagonalSum(matrix);
+       System.out.println(ans);
+    }
+}
