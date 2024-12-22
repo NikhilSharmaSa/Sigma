bffb888016e95956fa7ea3942f67241a6ec45cfe
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2024-12-22 10:39:22 +0530

    Basic operations on strings in java

---
+++ String/Basic.java
@@ -0,0 +1,10 @@
+package String;
+public class Basic {
+    public static void main(String[] args) {
+        String str="ABCD";
+        System.out.println(str.length());
+        String str2=new String("DEMO STRING");
+        System.out.println(str2);
+        System.out.println(str2.charAt(5));
+    }
+}
