13226a70b5a3791174f994427d07567c5dde94fc
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2024-12-24 20:25:19 +0530

    Basic of Bit and Bitwise Operators

---
+++ BitManipulation/BasicOfBit.java
@@ -0,0 +1,12 @@
+package BitManipulation;
+
+public class BasicOfBit {
+    public static void main(String[] args) {
+        System.out.println("5AND6: "+(5&6));
+        System.out.println("5OR6: "+(5|6));
+        System.out.println("5^6: "+(5^6));
+        System.out.println("~5: "+(~5));
+        System.out.println("5<<2: "+(5<<2));
+        System.out.println("5>>2: "+(5>>2));
+    }
+}
