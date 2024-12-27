be7c1c3d3198cb018c612ccce6d63f39eda88966
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2024-12-27 22:19:28 +0530

    KadaneAlgo

---
+++ Array-I/Revision/KadaneAlgo.java
@@ -0,0 +1,8 @@
+package Revision;
+
+public class KadaneAlgo {
+    public static void main(String[] args) {
+        int [] arr={-2,-3,4,-1,-2,1,5,-3};
+        System.out.println(maxSum(arr));
+    }
+}
