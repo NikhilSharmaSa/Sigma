b4ee2e795dfdbba6a3669ab811979587c0523826
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2025-01-03 21:34:20 +0530

    MergeSort

---
+++ BasicSorting/Revision/MergeSort.java
@@ -0,0 +1,26 @@
+package Revision;
+
+public class MergeSort {
+    public static void mergeS(int arr[],int start, int end){
+        if(start>end){
+            return ;
+        }
+        int mid=start+(end-start)/2;
+        mergeS(arr, start, mid);
+        mergeS(arr, mid+1, end);
+        mergeBoth(arr,start,mid,end);
+    }
+
+    public static void mergeBoth(int arr[], int start, int mid, int end){
+int [] temp=new int[end-start+1];
+
+
+    }
+    public static void main(String[] args) {
+        int [] arr={14,16,10,8,45,23};
+        mergeS(arr,0,arr.length-1);
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+}
