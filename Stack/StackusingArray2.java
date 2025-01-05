658721b5bfbb37bef4e7c3d524dd424fbc6e528c
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2025-01-06 01:33:16 +0530

    StackUsingArrAY

---
+++ Stack/StackusingArray2.java
@@ -0,0 +1,68 @@
+public class StackusingArray2 {
+
+
+    public static class Stack{
+int top;
+int capacity;
+int[] arr;
+public Stack(int length){
+arr=new int[length];
+this.capacity=length;
+this.top=-1;
+}
+
+boolean isEmpty(){
+    return top==-1;
+}
+
+
+boolean isFull(){
+    return top==capacity-1;
+}
+
+
+void push(int data){
+if(isFull()){
+    System.out.println("Stack is Full!");
+}else{
+    arr[++top]=data;
+    System.out.println(data +" Inserted Successfully !");
+}
+}
+
+void pop(){
+    if(isEmpty()){
+        System.out.println("The Stack is Empty!");
+    }else{
+        System.out.println(arr[top]+" Removed Success!");
+        arr[top--]=0;
+       
+       
+    }
+}
+
+
+void peek(){
+    if ((isEmpty())){
+        System.out.println("The Stack is Empty!");
+        
+    }else{
+System.out.println(arr[top]);
+    }
+}
+
+
+
+
+    }
+    public static void main(String[] args) {
+        Stack s1=new Stack(5);
+        s1.push(10);
+        s1.push(20);
+        s1.peek();
+        s1.pop();
+    s1.peek();
+    s1.pop();
+    s1.peek();
+    }
+}
